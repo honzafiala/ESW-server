4a72fcdc63200c80b5994e436f8790db03b572e1
honza <honza.fiala97@gmail.com>
2023-05-07 18:11:46 +0200

    Working state before changing epoll code

--- src/main.cpp
+++ src/main.cpp
@@ -9,11 +9,41 @@
 #include <thread>
 #include <vector>
 
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <fcntl.h>
+#include <string.h>
+
+
 using namespace std;
 
 void threadFunction(int threadId) {
-
+    while (1) {
+        ep.waitAndHandleEvents();
+    }
 }
+
+
+// void create_socket(int port_number) {
+//     printf("Binding to port %d\n", port);
+
+//         int sfd = socket(AF_INET, SOCK_STREAM, 0);
+//     if(sfd < 0){
+//         printf("Error while creating socket\n");
+//     }
+
+//     struct sockaddr_in receive_addr;
+//     receive_addr.sin_family = AF_INET;
+//     receive_addr.sin_port = htons(port);
+//     receive_addr.sin_addr.s_addr = INADDR_ANY;
+
+//     if(bind(sfd, (struct sockaddr*)&receive_addr, sizeof(receive_addr)) < 0){
+//         printf("Couldn't bind to the port\n");
+//     }
+//     listen(sfd, SOMAXCONN);
+//     return sfd;
+// }
 
 
 
@@ -27,20 +57,26 @@ int main(int argc, char *argv[])
     EpollInstance ep;
     EpollSocket socket(ep, atoi(argv[1]));
 
-    //  std::vector<std::thread> threads; // Vector to store thread objects
+     std::vector<std::thread> threads; // Vector to store thread objects
 
-    // for (int i = 0; i < atoi(argv[2]); ++i) {
-    //     threads.emplace_back(threadFunction, i);
-    // }
-
-    // // Wait for all threads to finish
-    // for (auto& thread : threads) {
-    //     thread.join();
-    // }
-
-        while (1) {
-        ep.waitAndHandleEvents();
+    for (int i = 0; i < atoi(argv[2]); ++i) {
+        threads.emplace_back(threadFunction, i);
     }
+
+    // Wait for all threads to finish
+    for (auto& thread : threads) {
+        thread.join();
+    }
+
+   // int socket_fd = create_socket(atoi(argv[2]));
+
+
+   // registerFd(EPOLLIN | EPOLLHUP);
+
+
+//     while (1) {
+//     ep.waitAndHandleEvents();
+// }
 
     return 0;
 }
